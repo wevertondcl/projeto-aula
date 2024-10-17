5a64e35cb774baa36397ebbb0b00a277bb26a497
Weverton Silva <wevertondcl@gmail.com>
2024-10-17 18:38:38 -0300

    comits até a aula 8

--- app/Livewire/HomeAgendamento.php
+++ app/Livewire/HomeAgendamento.php
@@ -7,10 +7,243 @@ use Livewire\Component;
 class HomeAgendamento extends Component
 {
     public $passoAtual = 1;
+    public $nome = '';
+    public $email = '';
+
+    public bool $corte = false;
+    public bool $barba = false;
+    public bool $sobrancelha = false;
+
+    public string $diaSelecionado = '';
+    public int $horarioSelecionado = 0;
+
+    public $diasDisponiveis = [
+       'domingo' => [
+           'nome' => 'Domingo',
+           'disponibilidade' => false
+       ],
+       'segunda' => [
+           'nome' => 'Segunda-feira',
+           'disponibilidade' => true
+       ],
+        'terca' => [
+          'nome' => 'Terça-feira',
+          'disponibilidade' => true
+        ],
+        'quarta' => [
+          'nome' => 'Quarta-feira',
+          'disponibilidade' => true
+        ],
+        'quinta' => [
+          'nome' => 'Quinta-feira',
+          'disponibilidade' => true
+        ],
+        'sexta' => [
+          'nome' => 'Sexta-feira',
+          'disponibilidade' => true
+        ],
+        'sabado' => [
+          'nome' => 'Sábado',
+          'disponibilidade' => false
+        ],
+    ];
+
+    public $horariosDisponiveis = [
+        'segunda' => [
+            [
+                'id' => 1,
+                'horario' => '08:00',
+                'disponibilidade' => true
+            ],
+            [
+                'id' => 2,
+                'horario' => '09:00',
+                'disponibilidade' => true
+            ],
+            [
+                'id' => 3,
+                'horario' => '10:00',
+                'disponibilidade' => false
+            ],
+        ],
+        'terca' => [
+            [
+                'id' => 200,
+                'horario' => '08:00',
+                'disponibilidade' => false
+            ],
+            [
+                'id' => 201,
+                'horario' => '09:00',
+                'disponibilidade' => true
+            ],
+            [
+                'id' => 205,
+                'horario' => '10:00',
+                'disponibilidade' => true
+            ],
+        ],
+        'quarta' => [
+            [
+                'id' => 300,
+                'horario' => '14:00',
+                'disponibilidade' => false
+            ],
+            [
+                'id' => 301,
+                'horario' => '16:00',
+                'disponibilidade' => true
+            ],
+            [
+                'id' => 305,
+                'horario' => '20:00',
+                'disponibilidade' => true
+            ],
+        ],
+        'quinta' => [
+            [
+                'id' => 400,
+                'horario' => '12:00',
+                'disponibilidade' => false
+            ],
+            [
+                'id' => 401,
+                'horario' => '13:00',
+                'disponibilidade' => true
+            ],
+            [
+                'id' => 405,
+                'horario' => '15:00',
+                'disponibilidade' => true
+            ],
+        ],
+        'sexta' => [
+            [
+                'id' => 700,
+                'horario' => '09:00',
+                'disponibilidade' => false
+            ],
+            [
+                'id' => 701,
+                'horario' => '11:00',
+                'disponibilidade' => true
+            ],
+            [
+                'id' => 702,
+                'horario' => '10:00',
+                'disponibilidade' => true
+            ],
+        ]
+    ];
+
+    public function messages(){
+        return [
+          //'nome.required' => 'Nome é obrigatório',
+        ];
+    }
+
+    public function validationAttributes(){
+        return [
+            'nome' => 'Nome',
+            'email' => 'E-mail',
+            'diaSelecionado' => 'Dia do agendamento',
+            'horarioSelecionado' => 'Horário do agendamento'
+        ];
+    }
+
+    public function updated($variavel)
+    {
+       if (str_starts_with($variavel,'diaSelecionado')){
+           $this->horarioSelecionado = 0;
+       }
+
+        if ($variavel === 'nome'){
+            $this->validate([
+                'nome' => 'required|min:10',
+            ]);
+        }
+
+        if ($variavel === 'email'){
+            $this->validate([
+                'email' => 'required|email',
+            ]);
+        }
+
+    }
 
     public function mudarPasso($passo = 1)
     {
         $this->passoAtual = $passo;
+    }
+    public function avancarPassoDois(){
+        $this->validate([
+            'nome' => 'required|min:10',
+            'email' => 'required|email',
+        ]);
+
+        $this->passoAtual = 2;
+    }
+
+    public function avancarPassoTres(){
+        $this->validate([
+            'nome' => 'required|min:10',
+            'email' => 'required|email',
+            'corte' => 'required|boolean',
+            'barba' => 'required|boolean',
+            'sobrancelha' => 'required|boolean',
+        ]);
+        if (!$this->validarProcedimentos()){
+            return;
+        }
+
+        $this->passoAtual = 3;
+        //dd($this->corte, $this->barba, $this->sobrancelha);
+    }
+
+    public function finalizarAgendamento()
+    {
+        $this->validate([
+            'nome' => 'required|min:10',
+            'email' => 'required|email',
+            'corte' => 'required|boolean',
+            'barba' => 'required|boolean',
+            'sobrancelha' => 'required|boolean',
+            'diaSelecionado' => 'required|string|in:domingo,segunda,terca,quarta,quinta,sexta,sabado',
+            'horarioSelecionado' => 'required|integer'
+        ]);
+
+        if (!$this->validarProcedimentos()){
+            $this->passoAtual = 2;
+            return;
+        }
+
+        if (!$this->horarioSelecionado) {
+            $this->addError('horarioSelecionado', 'Você deve selecionar um horário');
+            return;
+        }
+
+        // Filtrar o array para encontrar o item com o 'id' desejado e verificar se 'status' é true
+        $encontrado = array_filter($this->horariosDisponiveis[$this->diaSelecionado], function($horario) {
+            return $horario['id'] === $this->horarioSelecionado && $horario['disponibilidade'] === true;
+        });
+
+        if (empty($encontrado)) {
+            $this->addError('horarioSelecionado', 'Horário indisponível');
+            return;
+        }
+
+        $this->passoAtual = 4;
+
+    }
+
+    public function validarProcedimentos()
+    {
+        if (!$this->corte && !$this->barba && !$this->sobrancelha){
+            $this->addError('corte','Selecione ao menos um serviço');
+            return false;
+        }
+        return true;
+
     }
     public function render()
     {
--- resources/views/livewire/home-agendamento.blade.php
+++ resources/views/livewire/home-agendamento.blade.php
@@ -54,21 +54,210 @@
 
     <div class="mt-16">
         @if($passoAtual === 1)
-            <div class="flex mt-1 w-full mx-auto max-w-sm justify-center p-6 border border-gray-300 rounded-2xl">
-                <h2>Identificação</h2>
+            <div class="flex flex-col w-full mx-auto max-w-sm justify-center p-6 border border-gray-300 rounded-2xl">
+                <h2 class="text-lg font-bold text-indigo-600">Identificação</h2>
+
+                <form class="mt-5 space-y-6" wire:submit="avancarPassoDois">
+
+                    <div>
+                        <label for="nome" class="block text-sm font-medium leading-6 text-gray-900">Nome</label>
+                        <div class="relative mt-2 rounded-md shadow-sm">
+                            <input wire:model.live.debounce.600ms="nome" type="text" name="nome" id="nome"
+                                   @class([
+                                        'block w-full rounded-md border-0 py-1.5 pr-10 text-red-900 ring-1 ring-inset ring-red-300 placeholder:text-red-300 focus:ring-2 focus:ring-inset
+                                        focus:ring-red-500  sm:text-sm sm:leading-6' => $errors->has('nome'),
+                                        'block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400
+                                        focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' => !$errors->has('nome')
+                                    ])
+                                   placeholder="Digite seu nome" aria-invalid="true" aria-describedby="nome-error">
+                                @if($errors->has('nome'))
+                                    <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                                        <svg class="h-5 w-5 text-red-500" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+                                            <path fill-rule="evenodd" d="M18 10a8 8 0 1 1-16 0 8 8 0 0 1 16 0Zm-8-5a.75.75 0 0 1 .75.75v4.5a.75.75 0 0 1-1.5 0v-4.5A.75.75 0 0 1 10 5Zm0 10a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z" clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                @endif
+                        </div>
+                        @if($errors->has('nome'))
+                            <p class="mt-2 text-sm text-red-600" id="email-error">{{$errors->first('nome')}}</p>
+                        @endif
+                    </div>
+
+                    <div>
+                        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">E-mail</label>
+                        <div class="relative mt-2 rounded-md shadow-sm">
+                            <input wire:model.live.debounce.600ms="email" type="email" name="email" id="email"
+                                   @class([
+                                        'block w-full rounded-md border-0 py-1.5 pr-10 text-red-900 ring-1 ring-inset ring-red-300 placeholder:text-red-300 focus:ring-2 focus:ring-inset
+                                        focus:ring-red-500  sm:text-sm sm:leading-6' => $errors->has('email'),
+                                        'block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400
+                                        focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' => !$errors->has('email')
+                                    ])
+                                   placeholder="Digite seu e-mail" aria-invalid="true" aria-describedby="email-error">
+                            @if($errors->has('email'))
+                                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                                    <svg class="h-5 w-5 text-red-500" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+                                        <path fill-rule="evenodd" d="M18 10a8 8 0 1 1-16 0 8 8 0 0 1 16 0Zm-8-5a.75.75 0 0 1 .75.75v4.5a.75.75 0 0 1-1.5 0v-4.5A.75.75 0 0 1 10 5Zm0 10a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z" clip-rule="evenodd" />
+                                    </svg>
+                                </div>
+                            @endif
+                        </div>
+                        @if($errors->has('email'))
+                            <p class="mt-2 text-sm text-red-600" id="email-error">{{$errors->first('email')}}</p>
+                        @endif
+                    </div>
+
+
+                    <div>
+                        <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2
+                        focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Avançar</button>
+                    </div>
+                </form>
 
             </div>
         @endif
         @if($passoAtual === 2)
-            <div>
-                PASSO 2
-            </div>
+                <div class="flex flex-col w-full mx-auto max-w-sm justify-center p-6 border border-gray-300 rounded-2xl">
+                    <h2 class="text-lg font-bold text-indigo-600">{{$nome}}, escolha os procedimentos</h2>
+
+                    <form class="mt-5 space-y-6" wire:submit="avancarPassoTres">
+                        <fieldset>
+                            <legend class="sr-only">Notifications</legend>
+                            <div class="space-y-5">
+
+                                <div class="relative flex items-start">
+                                    <div class="flex h-6 items-center">
+                                        <input wire:model="corte" value="true" id="comments" aria-describedby="comments-description" name="comments" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600">
+                                    </div>
+                                    <div class="ml-3 text-sm leading-6">
+                                        <label for="comments" class="font-medium text-gray-900">Corte</label>
+                                    </div>
+                                </div>
+
+                                <div class="relative flex items-start">
+                                    <div class="flex h-6 items-center">
+                                        <input wire:model="barba" id="candidates" value="true" aria-describedby="candidates-description" name="candidates" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600">
+                                    </div>
+                                    <div class="ml-3 text-sm leading-6">
+                                        <label for="candidates" class="font-medium text-gray-900">Barba</label>
+                                    </div>
+                                </div>
+
+                                <div class="relative flex items-start">
+                                    <div class="flex h-6 items-center">
+                                        <input wire:model="sobrancelha" id="offers" value="true" aria-describedby="offers-description" name="offers" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600">
+                                    </div>
+                                    <div class="ml-3 text-sm leading-6">
+                                        <label for="offers" class="font-medium text-gray-900">Sobrancelha</label>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </fieldset>
+
+                        <div>
+                            @if($errors)
+                                @foreach($errors->all() as $error)
+                                    <p class="text-red-500">{{$error}}</p>
+                                @endforeach
+                            @endif
+                        </div>
+
+                        <div>
+                            <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2
+                            focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Avançar</button>
+                        </div>
+                    </form>
+
+                    <button type="button" wire:click="mudarPasso(1)"  class="mt-10 rounded bg-white px-2 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Voltar</button>
+
+                </div>
 
         @endif
         @if($passoAtual === 3)
-            <div>
-                PASSO 3
-            </div>
+                <div class="flex flex-col w-full mx-auto max-w-sm justify-center p-6 border border-gray-300 rounded-2xl">
+                    <h2 class="text-lg font-bold text-indigo-600">{{$nome}}, escolha o dia e horário</h2>
+
+                    <form class="mt-5 space-y-6" wire:submit="finalizarAgendamento">
+                        <fieldset aria-label="Choose a memory option">
+                            <div class="flex items-center justify-between">
+                                <div class="text-sm font-medium leading-6 text-gray-900">Dias disponíveis</div>
+                            </div>
+
+                            <div class="mt-2 grid grid-cols-3 gap-3">
+                                @foreach($diasDisponiveis as $chave => $dia)
+                                    <label
+                                           @class([
+                                                'flex items-center justify-center rounded-md px-3 py-3 text-xs font-semibold uppercase focus:outline-none sm:flex-1' => true,
+                                                'cursor-pointer' => $dia['disponibilidade'] === true,
+                                                'cursor-not-allowed opacity-25 ring-inset ring-1 ring-gray-300 bg-white text-gray-900' => $dia['disponibilidade'] === false,
+                                                'ring-indigo-600 ring-offset-2 ring-0 bg-indigo-600 text-white hover:bg-indigo-500' => $diaSelecionado === $chave,
+                                                'ring-1 ring-gray-300 bg-white text-gray-900 hover:bg-gray-50' => $diaSelecionado !== $chave && $dia['disponibilidade'] === true,
+
+                                            ])
+                                    >
+                                        <input wire:model.live="diaSelecionado" type="radio" name="memory-option" value="{{$chave}}" class="sr-only" @disabled($dia['disponibilidade'] === false)>
+                                        <span>{{$dia['nome']}}</span>
+                                    </label>
+                                @endforeach
+                            </div>
+                        </fieldset>
+
+                        <fieldset aria-label="Choose a memory option">
+                            <div class="flex items-center justify-between">
+                                <div class="text-sm font-medium leading-6 text-gray-900">Horários disponíveis</div>
+                            </div>
+
+                            <div class="mt-2 grid grid-cols-3 gap-3">
+                                @foreach($horariosDisponiveis as $chave => $horario)
+                                    @if($chave === $diaSelecionado)
+                                        @foreach($horario as $key => $opcao)
+                                            <label
+                                                    @class([
+                                                         'flex items-center justify-center rounded-md px-3 py-3 text-xs font-semibold uppercase focus:outline-none sm:flex-1' => true,
+                                                         'cursor-pointer' => $opcao['disponibilidade'] === true,
+                                                         'cursor-not-allowed opacity-25 ring-inset ring-1 ring-gray-300 bg-white text-gray-900' => $opcao['disponibilidade'] === false,
+                                                         'ring-indigo-600 ring-offset-2 ring-0 bg-indigo-600 text-white hover:bg-indigo-500' => $horarioSelecionado === $opcao['id'],
+                                                         'ring-1 ring-gray-300 bg-white text-gray-900 hover:bg-gray-50' => $horarioSelecionado !== $opcao['id'] && $opcao['disponibilidade'] === true,
+
+                                                     ])
+                                            >
+                                                <input wire:model.live="horarioSelecionado" type="radio" name="opcoes-horario" value="{{$opcao['id']}}" class="sr-only" @disabled($opcao['disponibilidade'] === false)>
+                                                <span>{{$opcao['horario']}}</span>
+                                            </label>
+                                        @endforeach
+                                    @endif
+                                @endforeach
+                            </div>
+                        </fieldset>
+
+                        <div>
+                            @if($errors)
+                                @foreach($errors->all() as $error)
+                                    <p class="text-red-500">{{$error}}</p>
+                                @endforeach
+                            @endif
+                        </div>
+
+                        <div>
+                            <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2
+                            focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Finalizar</button>
+                        </div>
+                    </form>
+
+                    <button type="button" wire:click="mudarPasso(2)"  class="mt-10 rounded bg-white px-2 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Voltar</button>
+                </div>
+        @endif
+        @if($passoAtual === 4)
+                <div class="flex flex-col w-full mx-auto max-w-sm justify-center p-6 border border-gray-300 rounded-2xl">
+                    <h2 class="text-lg font-bold text-indigo-600">{{$nome}}, agendamento realizado!</h2>
+                    <div class="flex justify-center max-auto">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-20 w-20 text-green-500">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                        </svg>
+
+                    </div>
+                </div>
         @endif
 
     </div>
