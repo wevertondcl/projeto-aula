ea770e04660af4e5feca0f4be74844459533242a
Weverton Silva <wevertondcl@gmail.com>
2024-11-16 18:35:02 -0300

    aula 27

--- resources/views/components/layouts/app-restrita.blade.php
+++ resources/views/components/layouts/app-restrita.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -17,19 +17,13 @@
     <!-- Styles -->
     @livewireStyles
 </head>
-<body>
+<body class="h-full">
 <div class="font-sans text-gray-900 antialiased">
     <main>
-        <!--
-  This example requires updating your template:
-
-  ```
-  <html class="h-full">
-  <body class="h-full">
-  ```
--->
         <div class="min-h-full">
-            <nav class="border-b border-gray-200 bg-white">
+            <nav class="border-b border-gray-200 bg-white"
+                 x-data="{menuMobile:false, menu:false}"
+            >
                 <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                     <div class="flex h-16 justify-between">
                         <div class="flex">
@@ -45,6 +39,7 @@
                                 <a href="#" class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700">Calendar</a>
                             </div>
                         </div>
+
                         <div class="hidden sm:ml-6 sm:flex sm:items-center">
                             <button type="button" class="relative rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
                                 <span class="absolute -inset-1.5"></span>
@@ -55,26 +50,25 @@
                             </button>
 
                             <!-- Profile dropdown -->
-                            <div class="relative ml-3">
+                            <div class="relative ml-3" x-on:click.outside="menu = false" x-cloak>
                                 <div>
-                                    <button type="button" class="relative flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                                    <button type="button" class="relative flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" id="user-menu-button" aria-expanded="false" aria-haspopup="true"
+                                        x-on:click="menu = !menu"
+                                    >
                                         <span class="absolute -inset-1.5"></span>
                                         <span class="sr-only">Open user menu</span>
                                         <img class="size-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
                                     </button>
                                 </div>
-
-                                <!--
-                                  Dropdown menu, show/hide based on menu state.
-
-                                  Entering: "transition ease-out duration-200"
-                                    From: "transform opacity-0 scale-95"
-                                    To: "transform opacity-100 scale-100"
-                                  Leaving: "transition ease-in duration-75"
-                                    From: "transform opacity-100 scale-100"
-                                    To: "transform opacity-0 scale-95"
-                                -->
-                                <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+                                <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1"
+                                        x-show="menu"
+                                        x-transition:enter="transition ease-out duration-200"
+                                        x-transition:enter-start="transform opacity-0 scale-95"
+                                        x-transition:enter-end="transform opacity-100 scale-100"
+                                        x-transition:leave="transition ease-in duration-75"
+                                        x-transition:leave-start="transform opacity-100 scale-100"
+                                        x-transition:leave-end="transform opacity-0 scale-95"
+                                >
                                     <!-- Active: "bg-gray-100 outline-none", Not Active: "" -->
                                     <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
                                     <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
@@ -82,17 +76,24 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="-mr-2 flex items-center sm:hidden">
+
+                        <div class="-mr-2 flex items-center sm:hidden" >
                             <!-- Mobile menu button -->
-                            <button type="button" class="relative inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" aria-controls="mobile-menu" aria-expanded="false">
+                            <button type="button" class="relative inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" aria-controls="mobile-menu" aria-expanded="false"
+                                x-on:click="menuMobile = !menuMobile"
+                            >
                                 <span class="absolute -inset-0.5"></span>
                                 <span class="sr-only">Open main menu</span>
                                 <!-- Menu open: "hidden", Menu closed: "block" -->
-                                <svg class="block size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
+                                <svg class="block size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon"
+                                    x-bind:class="{'hidden': menuMobile, 'block': !menuMobile}"
+                                >
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                                 </svg>
                                 <!-- Menu open: "block", Menu closed: "hidden" -->
-                                <svg class="hidden size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
+                                <svg class="hidden size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon"
+                                     x-bind:class="{'hidden': !menuMobile, 'block': menuMobile}"
+                                >
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
                                 </svg>
                             </button>
@@ -101,7 +102,18 @@
                 </div>
 
                 <!-- Mobile menu, show/hide based on menu state. -->
-                <div class="sm:hidden" id="mobile-menu">
+                <div class="sm:hidden" id="mobile-menu"
+                     x-show="menuMobile"
+                     x-transition:enter="transition ease-out duration-200"
+                     x-transition:enter-start="transform -translate-x-full opacity-0"
+                     x-transition:enter-end="transform translate-x-0 opacity-100"
+                     x-transition:leave="transition ease-in duration-75"
+                     x-transition:leave-start="transform translate-x-0 opacity-100"
+                     x-transition:leave-end="transform -translate-x-full opacity-0"
+                     x-on:click.outside="menuMobile = false"
+
+
+                >
                     <div class="space-y-1 pb-3 pt-2">
                         <!-- Current: "border-indigo-500 bg-indigo-50 text-indigo-700", Default: "border-transparent text-gray-600 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800" -->
                         <a href="#" class="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700" aria-current="page">Dashboard</a>
@@ -143,14 +155,12 @@
                 </header>
                 <main>
                     <div class="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
-                        <!-- Your content -->
+                        {{ $slot }}
                     </div>
                 </main>
             </div>
         </div>
-
     </main>
-    {{ $slot }}
 </div>
 
 @livewireScripts
